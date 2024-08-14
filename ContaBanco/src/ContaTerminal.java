e0429e6bc46fc2504cdd7f92f5b351674388f4b9
Caio Santana <caiovitorvs@hotmail.com>
2024-08-14 03:45:53 -0300

    resolvido

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Ola, para a criar sua conta iremos precisar de algumas informações");
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numero = S.nextInt();
+        S.nextLine();
+        System.out.println("Por favor, digite o número da Agência !");
+
+        String agencia = S.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente !");
+
+        String nomeCliente = S.nextLine();
+
+        System.out.println("Por favor, digite o valor inicial a ser depositado !");
+        double saldo = S.nextDouble();
+
+        System.out.printf("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é \""+agencia+"\", conta \""+numero+"\" e seu saldo de \'$%.2f\' já está disponível para saque.",saldo);
+    S.close();
     }
 }
